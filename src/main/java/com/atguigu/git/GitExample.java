ad173b4d679c9dbe2b2093df820c397059f745b5
zangxn <12774014@qq.com>
2022-09-08 09:04:34 +0800

    add Git2 v3 by client

--- src/main/java/com/atguigu/git/GitExample.java
+++ src/main/java/com/atguigu/git/GitExample.java
@@ -4,5 +4,7 @@ public class GitExample {
     public static void main(String[] args) {
         System.out.println("Hello Example");
         System.out.println("Hello Example2");
+        System.out.println("Hello Example3");
+
     }
 }
