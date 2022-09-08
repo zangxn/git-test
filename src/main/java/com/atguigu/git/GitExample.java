c3b8ce4071393038ee6cc8785461ec97f7c241b0
zangxn <12774014@qq.com>
2022-09-08 08:56:42 +0800

    add Git2 v2

--- src/main/java/com/atguigu/git/GitExample.java
+++ src/main/java/com/atguigu/git/GitExample.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class GitExample {
     public static void main(String[] args) {
         System.out.println("Hello Example");
+        System.out.println("Hello Example2");
     }
 }
